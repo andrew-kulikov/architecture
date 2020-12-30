ab28dd248471779307e27ae6a83ee437c30d7264
Andrew <andrew.1.kulikov@gmail.com>
2020-12-30 22:11:11 +0300

    Add task model

---
+++ Monolith/Monolith.Backend/Monolith.Backend/Monolith.Backend.Core/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Monolith.Backend.Core.Models
+{
+    public class Task
+    {
+        public Task(Guid id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
+
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+    }
+}
