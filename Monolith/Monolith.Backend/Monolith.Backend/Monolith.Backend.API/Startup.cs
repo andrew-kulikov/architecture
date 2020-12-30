cffa00c5d4a541caf6682eefaf566e9af92bbcf4
Andrew <andrew.1.kulikov@gmail.com>
2020-12-30 20:49:23 +0300

    Add sample monolith project

---
+++ Monolith/Monolith.Backend/Monolith.Backend/Monolith.Backend.API/Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Monolith.Backend.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            });
+        }
+    }
+}
