b08cd5fb4338b53bfa297924470dbb951036873b
Andrew <andrew.1.kulikov@gmail.com>
2021-01-01 15:42:08 +0300

    Move api url to envirinment variable

---
+++ Monolith/monolith-frontend/src/store/common/constants.ts
@@ -0,0 +1,1 @@
+export const API_ARL = process.env.REACT_APP_API_ARL;
--- Monolith/monolith-frontend/src/store/tasks/actions.ts
+++ Monolith/monolith-frontend/src/store/tasks/actions.ts
@@ -4,13 +4,14 @@ import { action } from 'typesafe-actions';
 import axios from 'axios';
 
 import { Constants } from './constants';
+import { API_ARL } from '../common/constants';
 import { ITask } from './types';
 
 export const getAllTasks = () => async (dispatch: Dispatch) => {
   dispatch(action(Constants.GET_ALL_TASKS, {}));
- 
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  const response = await axios.get<ITask[]>('https://localhost:5001/api/v1/tasks');
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await axios.get<ITask[]>(`${API_ARL}/api/v1/tasks`);
 
   if (response.status === 200) {
     return dispatch(setAllTasks(response.data));
